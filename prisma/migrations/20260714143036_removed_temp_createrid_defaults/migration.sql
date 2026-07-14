2c3b9691c16cacdac750595a2d64f9064c59d76f
humayun-saeed1 <humayunbaig046@gmail.com>
2026-07-14 19:31:02 +0500

    chore: remove default values for createrId in Actors, Director, and Movie tables

---
+++ prisma/migrations/20260714143036_removed_temp_createrid_defaults/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Actors" ALTER COLUMN "createrId" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "Director" ALTER COLUMN "createrId" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "Movie" ALTER COLUMN "createrId" DROP DEFAULT;
