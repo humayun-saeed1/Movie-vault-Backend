e9b9abbcc928809bc528a5e0d4d05fee137119e2
humayun-saeed1 <humayunbaig046@gmail.com>
2026-07-27 15:07:52 +0500

    refactor: remove unused project configuration files

--- src/watchlist/dto/update-watchlist.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateWatchlistDto } from './create-watchlist.dto.js';
-
-export class UpdateWatchlistDto extends PartialType(CreateWatchlistDto) {}
